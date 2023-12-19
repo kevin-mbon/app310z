129b09d1d59e9f2fefc5b6c54bf8d4466d023a6f
kevin-mbon <mbonikevin01@gmail.com>
2023-12-19 20:01:31 +0200

    initial commit

--- src/components/layout/store/FavoritesContext.js
+++ src/components/layout/store/FavoritesContext.js
@@ -1,13 +1,40 @@
-// import { createContext } from "react";
-// const FavoriteContext = createContext(
-//     {
-//         favorites:[],
-//         totalFavorites:0
-//     }
-// );
+import { createContext, useState } from "react";
+const FavoriteContext = createContext(
+    {
+        favorites:[],
+        totalFavorites:0,
+        addFavorite:(favoriteMeetUp) =>{},
+        removeFavorite:(meetupId) =>{},
+        itemIsFavorite: (meetupId) =>{}
+    }
+);
 
-function FavoritesContextProvider(){
+export function FavoritesContextProvider(props){
+    const[userFavorites,setUserFavorites ] = useState([]);
+    const context = {
+        favorites: userFavorites,
+        totalFavorites:userFavorites.length,
+        addFavorite: addFavoriteHandler,
+        removeFavorite: removeFavoriteHandler,
+        itemIsFavorite: itemIsFavoriteHandler
+    }
 
+    function addFavoriteHandler(favoriteMeetUp){
+        setUserFavorites((prevUserFavorites) => {
+       return prevUserFavorites.concat(favoriteMeetUp)
+        })
+    }
+
+    function removeFavoriteHandler(meetupId){
+        setUserFavorites(userFavorites.filter((meetup)=> meetup.id !== meetupId))
+    }
+
+    function itemIsFavoriteHandler(meetupId){
+        return userFavorites.some(meetup => meetup.id === meetupId);
+    }
+   return <FavoriteContext.Provider value={context}>
+    {props.children}
+   </FavoriteContext.Provider>
 }
 
-export default FavoritesContextProvider;
+export default FavoriteContext;
--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-
+import {FavoritesContextProvider} from './components/layout/store/FavoritesContext'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(
+root.render( 
+  <FavoritesContextProvider>
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
+  </FavoritesContextProvider>,
 );
 
 
